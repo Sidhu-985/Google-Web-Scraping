56681130d8216052253f32172f1d565abfd92cda
Sidharth sunil <sidharthsunil985@gmail.com>
2024-10-20 22:08:32 +0530

    Added correct css selectors to html function

--- main.go
+++ main.go
@@ -6,34 +6,42 @@ import (
 	"github.com/gocolly/colly"
 )
 
-func main() {
-    
-    c := colly.NewCollector()
+func main(){
+    c:=colly.NewCollector()
 
-    var input string
+    var searchQuery string
     fmt.Println("Enter the input:")
-    fmt.Scan(&input)
+    fmt.Scan(&searchQuery)
 
-    c.OnHTML("a[href]",func(h *colly.HTMLElement) {
-        fmt.Println(h.Text)
-        time.Sleep(1000*time.Millisecond)
+    GoogleScrape(c,searchQuery)
+}
+
+func GoogleScrape(c *colly.Collector,searchQ string){
+    c.SetRequestTimeout(time.Second*100)
+
+    c.OnHTML("div.a4bIc div.YacQ span",func(h *colly.HTMLElement) {
+    })
+
+    c.OnHTML("li.sbct.PZPZIf",func(h *colly.HTMLElement) {
+        result := h.ChildText(".wM6W7d span")
+        fmt.Println(result)
     })
 
     c.OnRequest(func(r *colly.Request) {
-        r.Headers.Set("Accept-Language","en-US;q=0.9")
-        fmt.Println("Visiting: ",r.URL)
+        fmt.Println("Visiting:",r.URL.String())
+        fmt.Println("Top 10 results")
     })
 
     c.OnError(func(r *colly.Response, err error) {
-        if err!=nil{
-            fmt.Println("No Error Occured")
-        }else{
-            fmt.Println(err)
-        }
+        fmt.Println("Error: ",err.Error())
+    })
+
+    c.OnResponse(func(r *colly.Response) {
+        
     })
 
     c.OnScraped(func(r *colly.Response) {
-        fmt.Println(r.StatusCode)
+        fmt.Println("Finished Scraping\nStatus Code:",r.StatusCode)
     })
 
     c.Visit("https://www.google.com/")
